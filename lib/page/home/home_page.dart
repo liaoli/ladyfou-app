5af12d0d2e92bfd140203ab648c6c6bf1b7f0e63
liaoli <277218962@qq.com>
2022-02-14 17:37:50 +0800

    banner

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:ladyfou/page/home/components/home_banner_swiper.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,8 +12,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("home"),
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 12,
+      ),
+      child: EasyRefresh.custom(
+        header: MaterialHeader(),
+        footer: MaterialFooter(),
+        onRefresh: () async {},
+        onLoad: () async {},
+        slivers: <Widget>[
+          SliverToBoxAdapter(child: homeBanner()),
+        ],
+      ),
     );
+  }
+
+  Widget homeBanner() {
+    return HomeBannerSwiper();
   }
 }
