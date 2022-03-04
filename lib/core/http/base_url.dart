5fbe957bdd5ceea2e8c063a219d58a89e4c9bb9e
liaoli <277218962@qq.com>
2022-03-04 14:51:39 +0800

    base url

--- lib/core/http/base_url.dart
+++ lib/core/http/base_url.dart
@@ -1,5 +1,5 @@
 /// 生产环境域名
-// const String baseUrl = "http://aashop.neverdown.cc";
+// const String baseUrl = "http://aashop.neverdown.cc/otonastyle";
 
 /// 测试环境域名
 // const String baseUrl = "https://release.ladyfou.com";
@@ -7,58 +7,63 @@
 /// 开发环境域名
 // const String baseUrl = "http://192.168.1.174";
 
-const String baseUrl = "https://release.ladyfou.com";
+const String baseUrl = "https://release.ladyfou.com/ladyfou";
+
+
+
+
+
 
 ///注册
-const String REGISTER_URI = "/ladyfou/account/register";
+const String REGISTER_URI = "/account/register";
 
 ///登录
-const String LOGIN_URI = "/ladyfou/account/login";
+const String LOGIN_URI = "/account/login";
 
 /// home 每日新品
-const String DAILY_NEW_PRODUCT_URI = "/ladyfou/product/newProduct";
+const String DAILY_NEW_PRODUCT_URI = "/product/newProduct";
 
 /// home 限时折扣
-const String DISCOUNT_PRODUCT_URI = "/ladyfou/product/discountProduct";
+const String DISCOUNT_PRODUCT_URI = "/product/discountProduct";
 
 /// 猜你喜欢
-const String YOU_LIKE_PRODUCT_URI = "/ladyfou/product/youLikeProduct";
+const String YOU_LIKE_PRODUCT_URI = "/product/youLikeProduct";
 
 ///获取分类
-const String CATEGORY_URI = "/ladyfou/catalog/category";
+const String CATEGORY_URI = "/catalog/category";
 
 /// 获取二级分类
-const String CATEGORY_PRODUCT_URI = "/ladyfou/catalog/category_product";
+const String CATEGORY_PRODUCT_URI = "/catalog/category_product";
 
 /// 获取一级二级分类数据
-const String CATE_CHILD_URI = "/ladyfou/catalog/cate_child";
+const String CATE_CHILD_URI = "/catalog/cate_child";
 
 ///获取订单列表
-const String ORDER_LIST_URI = "/ladyfou/account/order";
+const String ORDER_LIST_URI = "/account/order";
 
 /// 获取收藏列表
-const String WISH_LIST_URI = "/ladyfou/account/wish_list";
+const String WISH_LIST_URI = "/account/wish_list";
 
 /// 收藏
-const String WISH_PRODUCT_URI = "/ladyfou/account/wish_product";
+const String WISH_PRODUCT_URI = "/account/wish_product";
 
 /// 取消收藏
-const String DELETE_WISH_BATCH_URI = "/ladyfou/account/delete_wish_batch";
+const String DELETE_WISH_BATCH_URI = "/account/delete_wish_batch";
 
 /// 加入购物车
-const String ADD_TO_CART_URI = "/ladyfou/checkout/add_to_cart";
+const String ADD_TO_CART_URI = "/checkout/add_to_cart";
 
 /// 获取分类产品尺码规格
-const String GET_OPTION_SIZE_URI = "/ladyfou/catalog/getOptionSize";
+const String GET_OPTION_SIZE_URI = "/catalog/getOptionSize";
 
 /// 购物车列表
-const String SHOPPING_CART_LIST_URI = "/ladyfou/checkout/shopping_cart_list";
+const String SHOPPING_CART_LIST_URI = "/checkout/shopping_cart_list";
 
 /// 获取国家 省
-const String COUNTRY_LIST_URI = "/ladyfou/common/country";
+const String COUNTRY_LIST_URI = "/common/country";
 
 /// 获取市
-const String CITY_LIST_URI = "/ladyfou/common/city";
+const String CITY_LIST_URI = "/common/city";
 
 /// 获取区
-const String TOWN_LIST_URI = "/ladyfou/common/town";
+const String TOWN_LIST_URI = "/common/town";
